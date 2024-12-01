dd80477e9d47e32e1a3061d36a2246eba008f467
Ludmila-Maranhao-Lira <ludmila.lira@2025.icam.fr>
2024-12-01 16:02:33 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <PaginaInicial v-if="currentPage === 'home'" @aviso="irParaAviso"/>
+    <Aviso v-else-if="currentPage === 'aviso'" @iniciar="irParaFormulario"/>
+    <Formulario v-else />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import PaginaInicial from './components/PaginaInicial.vue';
+import Aviso from './components/Aviso.vue';
+import Formulario from './components/Formulario.vue';
+
+const currentPage = ref('home');
+
+const irParaAviso = () => {
+  currentPage.value = 'aviso';
+};
+
+const irParaFormulario = () => {
+  currentPage.value = 'formulario';
+};
+</script>
+
+<style scoped>
+
+</style>
